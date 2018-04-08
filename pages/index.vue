0207a9159c455731a58c5088d3229abc6979ad4d
lasers laptop <contact@template01.info>
2018-04-08 23:39:35 +0200

    initial push

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <p>
+      All my Books:
+    </p>
+    <nuxt-link v-for="book in books" :key="book.filename" :to="book.permalink" v-html="book.permalink"></nuxt-link>
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData ({ app, route }) {
+    return {
+      books: await app.$content('/books').getAll()
+    }
+  }
+}
+</script>
